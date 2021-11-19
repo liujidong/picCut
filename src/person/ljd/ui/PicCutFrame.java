6dd6bb5cc2d17f08aff5fd50a9672a9b72a6e1c9
liujidong <wwwliujidong@163.com>
2021-11-19 10:23:03 +0800

    测试及优化

--- src/person/ljd/ui/PicCutFrame.java
+++ src/person/ljd/ui/PicCutFrame.java
@@ -40,12 +40,14 @@ public class PicCutFrame extends JFrame implements ActionListener{
 	private JButton btn_line_ok = new JButton("标记线确定");
 	private JButton btn_cut	=	new JButton("切割");
 	private JButton btn_clear = new JButton("清空");
+	//JButton btn_test = new JButton("画直线");
 	private String imgPath;
 	BufferedImage image;
 	public PicCutFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
 		//---------------ui-------------------------------
 		this.setTitle("图片切割");
+		//setLocationRelativeTo(null);//窗体居中
 		Container c = this.getContentPane();
 		JToolBar toolbar = new JToolBar();
 		toolbar.add(btnOpen);
@@ -56,10 +58,12 @@ public class PicCutFrame extends JFrame implements ActionListener{
 				"image files", "jpg","jpeg","gif","png"));
 		
 		JPanel jp_east = new JPanel();
-		jp_east.setLayout(new GridLayout(4, 1));
+		jp_east.setLayout(new GridLayout(5, 1));
+		//jp_east.setPreferredSize(new Dimension(100, 250));
 		jp_east.add(btn_line_ok);
 		jp_east.add(btn_cut);
 		jp_east.add(btn_clear);
+		//jp_east.add(btn_test);
 		jp_east.add(new JLabel(softInfo()));
 		c.add(BorderLayout.EAST,jp_east);
 		//--------------event-------------------------------------
@@ -67,26 +71,28 @@ public class PicCutFrame extends JFrame implements ActionListener{
 		btn_line_ok.addActionListener(this);
 		btn_cut.addActionListener(this);
 		btn_clear.addActionListener(this);
-		
+		//btn_test.addActionListener(this);
+		//图片拖动处理
 		new DropTarget(c,new DropImgListener(this));
 		//pack();
-		setSize(1000,720);
+		setSize(1000,820);
+		setVisible(true);
 	}
 	public static String softInfo(){
 		StringBuffer infoHtml = new StringBuffer();
 		infoHtml.append("<html>\n");
 		//infoHtml.append(oldFile).append(":\n");
 		infoHtml.append("<ul>\n");
-		infoHtml.append("<li>liujidong2010@gmail.com</li>\n");
+		infoHtml.append("<li>593098937@qq.com</li>\n");
 		infoHtml.append("<li>制作：刘继东</li>\n");
-		infoHtml.append("<li>日期：2016-2</li>\n");
+		infoHtml.append("<li>日期：2021-11</li>\n");
 		infoHtml.append("</ul>\n");
 		infoHtml.append("</html>\n");
 		return infoHtml.toString();
 	}
 	public static void main (String []args)
 	{
-		new PicCutFrame().show();
+		new PicCutFrame();//.show();
 	}
 	public void showImage(String imgPath){
 		this.imgPath = imgPath;
@@ -115,7 +121,7 @@ public class PicCutFrame extends JFrame implements ActionListener{
 			
 			this.validate();
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
+			System.out.println("图片加载出错！");
 			e1.printStackTrace();
 		}
 	}
@@ -144,7 +150,9 @@ public class PicCutFrame extends JFrame implements ActionListener{
 	    		lsY.add(image.getHeight());
 			ImageCut.abscutBatch(imgPath, lsX, lsY);
 			JOptionPane.showMessageDialog(null, "操作成功！");
-		}
+		}/*else if(source == btn_test) {
+			lblImage.getGraphics().drawLine(10, 10, 200, 200);
+		}*/
 	}
 
 	class ChangeY implements ChangeListener{
